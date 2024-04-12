c50abb5129f8b0dbe9aede0a6e6d5d07bf4edb5c
Vitaliy Konchyts <vitaliy.konchyts@gmail.com>
2024-04-12 16:56:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -238,9 +238,9 @@ fi
 echo "" >> /home/${user}/cs2/game/csgo/cfg/secrets.cfg
 echo "echo \"secrets.cfg executed\"" >> /home/${user}/cs2/game/csgo/cfg/secrets.cfg
 
-echo "Merging in custom files from /home/${user}/cs2/${CUSTOM_FILES}/ to /home/${user}/cs2/game/csgo/"
+echo "------ Merging in custom files from /home/${user}/cs2/${CUSTOM_FILES}/ to /home/${user}/cs2/game/csgo/"
 cp -RT /home/${user}/cs2/${CUSTOM_FILES}/ /home/${user}/cs2/game/csgo/
-cp /home/${user}/cs2/${CUSTOM_FILES}/addons/counterstrikesharp/configs\ /admins.json home/${user}/cs2/game/csgo/addons/counterstrikesharp/configs/
+cp /home/${user}/cs2/${CUSTOM_FILES}/addons/counterstrikesharp/configs\ /admins.json home/${user}/cs2/game/csgo/addons/counterstrikesharp/configs/admins.json
 
 chown -R ${user}:${user} /home/${user}/cs2
 
